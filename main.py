dbd3d7b52e0766606abb9b2f289a414434b6943f
ArmandoArV <armando.arredondo.valle@gmail.com>
2022-05-11 09:35:47 -0500

    Cambios en el  main así como en los datos

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 """
-    ANALITICS MAIN
+    ANALYTICS MAIN
+    
+    Note: We're simply focusing on USA due it's the nearest country to Mexico, here we could theorize how it could,
+    affect the economy in Mexico.
+    
 """
 import pandas as pd
 import numpy as np
+import statistics as stat
 
 usa = pd.read_csv(r"DB\United States 10-Year Bond Yield.csv")
 
@@ -10,15 +15,44 @@ usa = pd.read_csv(r"DB\United States 10-Year Bond Yield.csv")
 # print(usa) # Here we simply print the csv
 # print(usa.head(6)) # Here we take the very first 5 rows of data
 
-# Dataframes de el precio asi como el cambio
-PrecioUSA = usa['Price'] # Dataframe for the price
-CambioUSA = usa['Change %'] # Dataframe for the Change in percentage
-print(PrecioUSA)
+# Dataframe
+PriceUSA = usa['Price'] # Dataframe for the price
+ChangeUSA = usa['Change %'] # Dataframe for the Change in percentage
+# print(PriceUSA)
 
 # Some statistics stuff
-#AvPrecio = np.average(PrecioUSA)
-# AvCambio = CambioUSA.mean()
+
+# Change
+AvChange = ChangeUSA.mean()
+MAXChange = np.max(ChangeUSA)
+MINChange = np.min(ChangeUSA)
+RanChange = MAXChange-MINChange
+VarChange = stat.variance(ChangeUSA)
+# Price
+AvPrice = np.average(PriceUSA)
+MAXPrice = np.max(PriceUSA)
+MINPrice = np.min(PriceUSA)
+RanPrice = MAXPrice-MINPrice
+VarPrice = stat.variance(PriceUSA)
 
 # Here we print the statistics stuff
-#print("Average of prices: ",AvPrecio)
-# print("Average of change: ",AvCambio)
+print(" ")
+print("STATISTICS:") # Simply a title
+print("___________________________________________________ ")
+print(" ")
+print("COLUMN PRICE: ")
+print(" ")
+print("Average of prices: ",AvPrice)
+print("Max value of the price column: ",MAXPrice)
+print("Min value of the price column: ",MINPrice)
+print("Range of the price column: ",RanPrice)
+print("Variance of the price column: ",VarPrice)
+print("___________________________________________________")
+print("COLUMN CHANGE: ")
+print(" ")
+print("Average of change: ",AvChange)
+print("Max value of the change column: ",MAXChange)
+print("Min value of the change column",MINChange)
+print("Range of the change column:",RanChange)
+print("Variance of the change column: ",VarChange)
+print(" ")
